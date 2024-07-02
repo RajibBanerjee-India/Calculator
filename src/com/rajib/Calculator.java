25823a6229f4b7162a53285cc1296d8d94896eae
Rajib Banerjee <rb021990@gmail.com>
2024-07-03 00:00:22 +0530

    Initial commit

---
+++ src/com/rajib/Calculator.java
@@ -0,0 +1,8 @@
+package com.rajib;
+
+public class Calculator {
+
+    public int integerDivision(int dividend, int divisor) {
+        return dividend/divisor;
+    }
+}
---
+++ test/com/rajib/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.rajib;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void integerDivision() {
+        fail("No implementation provided.");
+    }
+}
